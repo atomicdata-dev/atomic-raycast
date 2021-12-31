964203ec8e5eac79a6ce345abdb0483c557cbe79
Joep Meindertsma <joep@ontola.io>
2021-12-31 18:09:17 +0100

    Basic search working

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ _Note: this extension is beta and not yet available in the Raycast store_
 ## Running locally
 
 ```sh
+# make sure you have Raycast and Yarn / NPM installed
 yarn
+# Start local server
 yarn dev
+# Now open raycast. The dev tool should be the top item!
 ```
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,78 @@
-import { ActionPanel, Detail, List, PushAction } from "@raycast/api";
+import { ActionPanel, Detail, List, OpenInBrowserAction, PushAction } from "@raycast/api";
+import { useServerSearch, StoreContext, useTitle, useResource, useString } from '@tomic/react';
+import { Store, urls } from '@tomic/lib';
+import React from "react";
+
+const store = new Store({
+  // By setting a Base URL, we tell the client where to send Commits to.
+  serverUrl: "https://atomicdata.dev"
+});
 
 export default function Command() {
+
   return (
-    <List>
-      <List.Item
-        key="1"
-        icon="list-icon.png"
-        title="Greeting"
-        actions={
-          <ActionPanel>
-            <PushAction title="Show Details" target={<Detail markdown="# Hey! 👋" />} />
-          </ActionPanel>
-        }
-      />
-    </List>
+    <StoreContext.Provider value={store}>
+      <Content />
+    </StoreContext.Provider>
   );
+}
+
+function Content() {
+  const [query, setQuery] = React.useState("");
+  const { results, loading, error } = useServerSearch(query, {
+    include: true,
+  });
+
+  const onSearchTextChange = async (text: string) => {
+    setQuery(text);
+  }
+
+  return (
+    <List
+      navigationTitle="Atomic Data Search"
+      searchBarPlaceholder="Search your Atomic Server..."
+      isLoading={loading}
+      onSearchTextChange={onSearchTextChange}
+    >
+      {error && <List.Item title={error.message} />}
+      {query.length > 0 && results && results.map(subject => (
+        <SearchHit
+          subject={subject}
+          key={subject}
+        />
+      ))}
+    </List>
+  )
+}
+
+interface SearchHitProps {
+  subject: string,
+}
+
+function SearchHit({ subject }: SearchHitProps) {
+  const resource = useResource(subject);
+
+  const title = useTitle(resource);
+  const [description] = useString(resource, urls.properties.description);
+
+  const klass = useResource(resource.getClasses()[0]);
+  const klassTitle = useTitle(klass);
+
+  const md = `# ${title} \n\n ${description}`;
+
+  return (
+    <List.Item
+      key={subject}
+      icon="list-icon.png"
+      title={title}
+      subtitle={description || ""}
+      accessoryTitle={klassTitle}
+      actions={
+        <ActionPanel>
+          <PushAction title="Show Details" target={<Detail markdown={md} actions={<ActionPanel><OpenInBrowserAction title="Open in browser" url={subject} /></ActionPanel>}/>} />
+          <OpenInBrowserAction title="Open in browser" url={subject} />
+        </ActionPanel>
+      }
+    />
+  )
 }
